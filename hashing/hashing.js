e0ee12bac88ba30e8865ac8bdefcde909753738f
Martijn <martijncvv@gmail.com>
2021-09-10 17:17:20 +0200

    exercise part one and two

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -1,10 +1,10 @@
 "use strict";
 
-var crypto = require("crypto");
+let crypto = require("crypto");
 
 // The Power of a Smile
 // by Tupac Shakur
-var poem = [
+let poem = [
 	"The power of a gun can kill",
 	"and the power of fire can burn",
 	"the power of wind can chill",
@@ -15,29 +15,84 @@ var poem = [
 	"especially yours can heal a frozen heart",
 ];
 
-var Blockchain = {
+let blockchain = {
 	blocks: [],
 };
 
 // Genesis block
-Blockchain.blocks.push({
+blockchain.blocks.push({
 	index: 0,
 	hash: "000000",
 	data: "",
 	timestamp: Date.now(),
 });
 
-// TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+// insert each line into blockchain
+for (let line of poem) {
+	blockchain.blocks.push(createBlock(line));
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+console.log(`blockchain is valid: ${verifyChain(blockchain)}`);
 
+function createBlock(line) {
+	let data = line;
+	let index = blockchain.blocks.length;
+	let prevHash = blockchain.blocks[blockchain.blocks.length - 1].hash;
+	let timestamp = Date.now();
 
-// **********************************
+	let hash = blockHash(`${index} ${prevHash} ${data} ${timestamp}`);
+
+	return {
+		index,
+		data,
+		prevHash,
+		timestamp,
+		hash,
+	};
+}
 
 function blockHash(bl) {
-	return crypto.createHash("sha256").update(
-		// TODO: use block data to calculate hash
-	).digest("hex");
+	return crypto.createHash("sha256").update(bl).digest("hex");
+}
+
+function verifyChain(blockchain) {
+	let prevBlockHash = "000000";
+
+	for (let block of blockchain.blocks) {
+		// genesis block check
+		if (block.index == 0 && block.hash == "000000") {
+			console.log(`Genesis block verified`);
+			continue;
+		} else if (block.index == 0 && block.hash != "000000") {
+			return false;
+		}
+
+		// block data check
+		if (
+			block.data &&
+			block.prevHash &&
+			block.index >= 0 &&
+			block.hash ===
+				blockHash(
+					`${block.index} ${block.prevHash} ${block.data} ${block.timestamp}`
+				)
+		) {
+			console.log(`Block #${block.index} verified`);
+		} else {
+			console.log(`Error r81, node ${block.index}`);
+			return false;
+		}
+
+		// checks block link
+		if (block.index > 0 && block.prevHash !== prevBlockHash) {
+			console.log(`Error r87, node ${block.index}`);
+			return false;
+		}
+		console.log(`hash: ${block.prevHash}\nprevHash: ${prevBlockHash} \n`);
+
+		prevBlockHash = block.hash;
+	}
+
+	console.log("BLOCKCHAIN VERIFIED");
+	return true;
 }
